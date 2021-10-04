6d3e777e4712f9f85a01b73ee409318436b5b255
MayNearPPP <maynearppp@gmail.com>
2021-10-04 10:25:45 +0800

    Run success

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 # Demo Converter v1.2
 
 import os
-import moviepy
-import PIL
+# import moviepy
+# import PIL
 import shutil
 from moviepy.editor import *
 from PIL import Image
 
-path = os.path.dirname(os.path.dirname(os.path.realpath(sys.executable)))
+# path = os.path.dirname(os.path.dirname(os.path.realpath(sys.executable)))
 
 INPUT_FILES = input("输入文件：")
 
-INPUT_FILES = INPUT_FILES.replace('/', '\\')
-dirName, oringinName = os.path.split(INPUT_FILES)
-footageName = oringinName.replace('.mov', '')
-sequenceDirName = dirName+f'\\_{footageName}Sequence'
+INPUT_FILES = INPUT_FILES.replace("'", "")
+dirName, originName = os.path.split(INPUT_FILES)
+footageName = originName.replace('.mov', '')
+sequenceDirName = dirName+f'/_{footageName}_Sequence'
 os.mkdir(sequenceDirName)
 outputVideoName = footageName + '_Demo.mp4'
 outputVideo = os.path.join(dirName, outputVideoName)
 
 # print(dirName)
-# print(sequenceName)
+# print(originName)
 # print(footageName)
+# print(sequenceDirName)
 # print(outputVideoName)
 # print(outputVideo)
+
 
 # 转JPG序列
 clip = VideoFileClip(INPUT_FILES)
@@ -36,13 +38,12 @@ for i, frame in enumerate(clip.iter_frames()):
     newImg.save(newSequenceFilepath)
     print(f"{i} ")
 
+
 # 转MP4视频
 
 this_dir = os.listdir(sequenceDirName)
 filepaths = [os.path.join(sequenceDirName, fname)
              for fname in this_dir if fname.endswith("jpg")]
-
-# print(filepaths)
 
 directory = {}
 
@@ -66,6 +67,10 @@ for k in sorted(directory.keys()):
 clip = ImageSequenceClip(newPaths, fps)
 clip.write_videofile(outputVideo)
 
+
+# mp4 to m4v
 os.rename(outputVideo, outputVideo.replace(".mp4", ".m4v"))
 
+
+# Delete Sequence
 shutil.rmtree(sequenceDirName)
